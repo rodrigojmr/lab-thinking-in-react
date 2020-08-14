103dab39f13d950ef8812e1aae0ea35763e239ac
Rodrigo Moura <rodrigojorgemoura@gmail.com>
2020-08-14 12:39:26 +0100

    CSS

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -3,20 +3,24 @@ import React from 'react';
 function SearchBar(props) {
   return (
     <div className="search">
-      Search
-      <input
-        type="text"
-        name="searchQuery"
-        value={props.searchVal}
-        onChange={(event) => props.search(event)}
-      />
-      <input
-        onChange={props.toggleStock}
-        id="input-checkbox"
-        name="in-stock"
-        type="checkbox"
-      />
-      <label htmlFor="input-checkbox">Only show products on stock</label>
+      <div>
+        Search
+        <input
+          type="text"
+          name="searchQuery"
+          value={props.searchVal}
+          onChange={(event) => props.search(event)}
+        />
+      </div>
+      <div>
+        <input
+          onChange={props.toggleStock}
+          id="input-checkbox"
+          name="in-stock"
+          type="checkbox"
+        />
+        <label htmlFor="input-checkbox">Only show products on stock</label>
+      </div>
     </div>
   );
 }
